a0b20f1c06c2853763b87f5e04b669893c76116a
Kevin Kenfack <kenfackjoelkevin@yahoo.com>
2025-05-08 14:49:58 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="min-h-screen">
+    <div class="space-y-24">
+      <HomeIntro />
+      <HomeSocialLinks />
+      <HomeFeaturedProjects />
+      <HomeFeaturedArticles />
+      <HomeTestimonials/>
+      <HomeNewsletter />
+    </div>
+  </main>
+</template>
